efe33be1388923af7ee15c52c9e7d576e8aa2589
BlackRain <masaraksh86@mail.ru>
2023-04-15 18:24:16 +0700

    Загрузка списка карточек из массива

--- src/components/blocks/CardsWrapper/CardsWrapper.jsx
+++ src/components/blocks/CardsWrapper/CardsWrapper.jsx
@@ -2,86 +2,26 @@ import React from "react";
 
 import styles from './cardswrapper.module.css';
 
+import productList from "../../../products";
 import Card from '../Card/Card';
-import image1 from '../../../assets/card1.png';
-import image2 from '../../../assets/card2.png';
-import image3 from '../../../assets/card3.png';
-import image4 from '../../../assets/card4.png';
 
 function CardsWrapper() {
     return (
         <div className={styles["card-list"]}>
-            <Card
-                image={image1}
-                title="Устрицы по рокфеллеровски"
-                description="Значимость этих проблем настолько очевидна, что укрепление и развитие структуры"
-                cost={2700}
-                weight={500}
-                pcs={false}
-            />
-
-            <Card
-                image={image2}
-                title="Свиные ребрышки на гриле с зеленью"
-                description="Не следует, однако забывать, что реализация намеченных плановых"
-                cost={1600}
-                weight={750}
-                pcs={false}
-            />
-
-            <Card
-                image={image3}
-                title="Креветки по-королевски в лимонном соке"
-                description="Значимость этих проблем настолько очевидна, что укрепление и развитие структуры обеспечивает широкому кругу"
-                cost={1820}
-                weight={7}
-                pcs={true}
-            />
-
-            <Card
-                image={image4}
-                title="Устрицы по рокфеллеровски"
-                description="Значимость этих проблем настолько очевидна, что укрепление и развитие структуры"
-                cost={2700}
-                weight={500}
-                pcs={false}
-            />
-
-            <Card
-                image={image1}
-                title="Устрицы по рокфеллеровски"
-                description="Значимость этих проблем настолько очевидна, что укрепление и развитие структуры"
-                cost={2700}
-                weight={500}
-                pcs={false}
-            />
-
-            <Card
-                image={image2}
-                title="Свиные ребрышки на гриле с зеленью"
-                description="Не следует, однако забывать, что реализация намеченных плановых"
-                cost={1600}
-                weight={750}
-                pcs={false}
-            />
-
-            <Card
-                image={image3}
-                title="Креветки по-королевски в лимонном соке"
-                description="Значимость этих проблем настолько очевидна, что укрепление и развитие структуры обеспечивает широкому кругу"
-                cost={1820}
-                weight={7}
-                pcs={true}
-            />
-
-            <Card
-                image={image4}
-                title="Устрицы по рокфеллеровски"
-                description="Значимость этих проблем настолько очевидна, что укрепление и развитие структуры"
-                cost={2700}
-                weight={500}
-                pcs={false}
-            />
+            {
+                productList.map(element => {
+                    return (
+                        <Card
+                            key={element.id}
+                            image={element.imageUrl}
+                            title={element.title}
+                            description={element.description}
+                            cost={element.cost}
+                            weight={element.weight ? element.weight : element.pieces}
+                            pcs={element.pieces}
+                        />
+                    )
+                })};
         </div>
     )
 }
