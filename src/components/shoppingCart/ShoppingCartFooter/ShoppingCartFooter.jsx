a618bcb8254d47dc4a44629a967ec91b1f658ebd
BlackRain <masaraksh86@mail.ru>
2023-04-27 18:25:03 +0700

    Исправлена вёрстка корзины

--- src/components/shoppingCart/ShoppingCartFooter/ShoppingCartFooter.jsx
+++ src/components/shoppingCart/ShoppingCartFooter/ShoppingCartFooter.jsx
@@ -1,14 +1,10 @@
 import React from "react";
 
-import imageLine from "./../../../assets/line.svg";
-
 import styles from "./shoppingcartfooter.module.css";
 
 function ShoppingCartFooter({totalPrice}) {
     return (
         <div className={styles["footer"]}>
-            <img src={imageLine} alt="" />
-
             <div className={styles["footer__content-wrapper"]}>
                 <div className={styles["footer__text-wrapper"]}>
                     <p>
