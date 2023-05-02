198745df9494edc01b38d4ab47989edb211f5b4c
BlackRain <masaraksh86@mail.ru>
2023-05-02 15:17:23 +0700

    Отображение данных в корзине при помощи redux toolkit

--- src/components/shoppingCart/ShoppingCartFooter/ShoppingCartFooter.jsx
+++ src/components/shoppingCart/ShoppingCartFooter/ShoppingCartFooter.jsx
@@ -1,8 +1,11 @@
 import React from "react";
+import { useSelector } from "react-redux";
 
 import styles from "./shoppingcartfooter.module.css";
 
-function ShoppingCartFooter({totalPrice}) {
+function ShoppingCartFooter() {
+    const totalPrice = useSelector(state => state.products.totalPrice);
+
     return (
         <div className={styles["footer"]}>
             <div className={styles["footer__content-wrapper"]}>
